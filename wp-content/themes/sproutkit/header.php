3c36bfdce2db8a70707325f96bde807d67b3ce96
Scrivs <scrivs@gmail.com>
2016-10-09 20:48:51 -0700

    init

--- wp-content/themes/sproutkit/header.php
+++ wp-content/themes/sproutkit/header.php
@@ -5,7 +5,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="icon" type="image/png" href="https://s3-us-west-2.amazonaws.com/talentedunderachiever-master/wp-content/uploads/2016/10/09214323/mini-avatar.png" data-pin-nopin="true">
+		<link rel="icon" type="image/png" href="https://s3-us-west-2.amazonaws.com/sproutkit-master/wp-content/uploads/2016/10/10034336/favicon.png" data-pin-nopin="true">
 		<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 		<?php endif; ?>
@@ -13,9 +13,12 @@
 		<?php wp_head(); ?>
 		<script src="https://use.typekit.net/ykl1zpm.js"></script>
         <script>try{Typekit.load();}catch(e){}</script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	</head>
-	<body>
+	<body class="basic">
+		<div class="border top"></div>
+		<div class="border left"></div>
+		<div class="border bottom"></div>
+		<div class="border right"></div>
 		<script
 		    type="text/javascript"
 		    async defer
@@ -23,13 +26,13 @@
 		></script>
 		<div class="title-bar" data-responsive-toggle="mc-menu" data-hide-for="medium">
 		  <button class="menu-icon" type="button" data-toggle></button>
-		  <div class="title-bar-title"><img src="/assets/img/yellow-logo.png"></div>
+		  <div class="title-bar-title"><img src="https://s3-us-west-2.amazonaws.com/sproutkit-master/wp-content/uploads/2016/10/10034338/yellow-logo.png"></div>
 		</div>
 
 		<div class="top-bar" data-topbar id="mc-menu">
 		    <div class="top-bar-left">
 		      <ul class="dropdown menu" data-dropdown-menu>
-		          <li class="hide-for-small-only"><a href="/"><img src="/assets/img/yellow-logo.png"></a></li>
+		          <li class="hide-for-small-only"><a href="/"><img src="https://s3-us-west-2.amazonaws.com/sproutkit-master/wp-content/uploads/2016/10/10034338/yellow-logo.png"></a></li>
 		          <li class="menu-text hide-for-small-only"><a href="/">SproutKit</a></li>
 		          <li><a href="/work">Our Work</a></li>
 		          <li><a href="/blog">Blog</a></li>
