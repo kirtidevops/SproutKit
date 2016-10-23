824531b2e898d6c23bc2a30f3bd66f70b49f47de
Scrivs <scrivs@gmail.com>
2016-10-23 13:58:37 -0700

    init

--- wp-content/themes/sproutkit/page.php
+++ wp-content/themes/sproutkit/page.php
@@ -1,17 +1,15 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<section class="lead-single">
-	<div class="lead-container">
-		<div class="row">
-			<div class="small-12 columns">
-				<?php the_title( '<h1 class="entry-title text-center">', '</h1>' ); ?>
-			</div>
+<section class="hero">
+	<div class="row">
+		<div class="small-12 columns">
+			<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 		</div>
 	</div>
 </section>
 <section class="article">
 	<div class="row">
-		<div class="small-12 columns">
+		<div class="small-12 large-8 end columns">
 			<?php the_content(); ?>
 		</div>
 	</div>
---
+++ wp-content/themes/sproutkit/template-services.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: Services
+*/
+?>
+
+<?php get_header(); ?>
+<section class="hero">
+	<div class="row">
+		<div class="small-12 columns">
+			<h2>Services We Offer</h2>
+			<h3 class="subheader">Let&rsquo;s Work Together in Building Something Epic</h3>
+		</div>
+	</div>
+</section>
+<section class="content">
+	<div class="row">
+		<div class="small-12 large-6 columns">
+			<h2><a href="/blog-takeover">Blog Takeover - $499</a></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe sequi nesciunt esse quaerat consequatur quisquam odit quidem voluptas similique perspiciatis eaque veritatis, error ratione rem adipisci, ipsa odio excepturi.</p>
+			<p><a href="/blog-takeover" class="button">Learn more</a></p>
+		</div>
+		<div class="small-12 large-6 columns">
+			<h2><a href="/pinterest-takeover">Pinterest Takeover - $129</a></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe sequi nesciunt esse quaerat consequatur quisquam odit quidem voluptas similique perspiciatis eaque veritatis, error ratione rem adipisci, ipsa odio excepturi.</p>
+			<p><a href="/blog-takeover" class="button">Learn more</a></p>
+		</div>
+		<div class="small-12 large-6 end columns">
+			<h2><a href="/custom-solutions">Custom Solutions</a></h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe sequi nesciunt esse quaerat consequatur quisquam odit quidem voluptas similique perspiciatis eaque veritatis, error ratione rem adipisci, ipsa odio excepturi.</p>
+			<p><a href="/blog-takeover" class="button">Learn more</a></p>
+		</div>
+	</div>
+</section>
+<?php get_footer(); ?>
