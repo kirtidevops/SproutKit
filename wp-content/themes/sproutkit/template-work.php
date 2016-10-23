0f2c45c2158187ee69514828341052dfbc29fecf
Scrivs <scrivs@gmail.com>
2016-10-23 16:29:48 -0700

    init

--- wp-content/themes/sproutkit/template-work.php
+++ wp-content/themes/sproutkit/template-work.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Services
+Template Name: Work
 */
 ?>
 
@@ -8,30 +8,30 @@ Template Name: Services
 <section class="hero">
 	<div class="row">
 		<div class="small-12 columns">
-			<h2>Services We Offer</h2>
-			<h3 class="subheader">Let&rsquo;s Work Together in Building Something Epic</h3>
+			<h2>The Work We&rsquo;ve Done</h2>
+			<h3 class="subheader">Words Are Good, Action Is Better</h3>
 		</div>
 	</div>
 </section>
 <section class="content">
 	<div class="row" data-equivalent>
 		<div class="small-12 large-6 columns" data-equivalent-watch>
-			<h2><a href="/blog-takeover">Blog Takeover - $499</a></h2>
+			<h2><a href="/blog-takeover">Obstacle.co</a></h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe sequi nesciunt esse quaerat consequatur quisquam odit quidem voluptas similique perspiciatis eaque veritatis, error ratione rem adipisci, ipsa odio excepturi.</p>
 			<p><a href="/blog-takeover" class="button">Learn more</a></p>
 		</div>
 		<div class="small-12 large-6 columns" data-equivalent-watch>
-			<h2><a href="/pinterest-takeover">Pinterest Takeover - $129</a></h2>
+			<h2><a href="/pinterest-takeover">Thrive/Strive</a></h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe sequi nesciunt esse quaerat consequatur quisquam odit quidem voluptas similique perspiciatis eaque veritatis, error ratione rem adipisci, ipsa odio excepturi.</p>
 			<p><a href="/blog-takeover" class="button">Learn more</a></p>
 		</div>
 		<div class="small-12 large-6 columns" data-equivalent-watch>
-			<h2><a href="/pinterest-takeover">Content Writing - Starts at $299</a></h2>
+			<h2><a href="/pinterest-takeover">Struggle.co</a></h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe sequi nesciunt esse quaerat consequatur quisquam odit quidem voluptas similique perspiciatis eaque veritatis, error ratione rem adipisci, ipsa odio excepturi.</p>
 			<p><a href="/blog-takeover" class="button">Learn more</a></p>
 		</div>
 		<div class="small-12 large-6 columns" data-equivalent-watch>
-			<h2><a href="/custom-solutions">Custom Solutions</a></h2>
+			<h2><a href="/custom-solutions">Talented Underachiever</a></h2>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid saepe sequi nesciunt esse quaerat consequatur quisquam odit quidem voluptas similique perspiciatis eaque veritatis, error ratione rem adipisci, ipsa odio excepturi.</p>
 			<p><a href="/blog-takeover" class="button">Learn more</a></p>
 		</div>
